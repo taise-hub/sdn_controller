c2f61102151408741cfdeacd3e2bf574570a13b6
井上大誠 <t.base.8901@icloud.com>
2021-09-10 15:04:39 +0900

    imple topsis

---
+++ topsis/topsis.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+class Topsis():
+    request_matrics =np.array(
+        [1, 1, 1/3, 1/3, 1, 1/6, 1],
+        [1, 1, 1/3, 1/3, 1, 1/6, 1],
+        [3, 3, 1, 1, 3, 1/6, 3],
+        [1, 1, 1/3, 1/3, 1, 1/6, 1],
+        [6, 6, 6, 6, 6, 1, 6],
+        [1, 1, 1/3, 1/3, 1, 1/6, 1])
+    def calc():
+        print("AAA")
+        print(request_matrics)
+
+
+
+if __name__ == '__main__':
+    print("AAAAAAAAAA")
+    t = Topsis()
+    t.calc()
